e5d4c1a67e94bb940e59de0f3850a8ff7e9e822c
wlft <100052562+wlft@users.noreply.github.com>
2026-05-24 09:59:56 +1000

    feat: add favicon

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -7,7 +7,7 @@ export default function Nav() {
                 <div className="relative flex h-12 items-center justify-between mx-4">
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start container mx-auto">
                         <Link className="flex shrink-0 items-center gap-3 font-bold" href='/'>
-                            Baseplate
+                            Baseplate.wiki
                         </Link>
                     </div>
                 </div>
