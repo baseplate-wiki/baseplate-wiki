240359cbee4071484197d8ea119329bd391dd2d3
wlft <100052562+wlft@users.noreply.github.com>
2026-05-24 10:33:29 +1000

    fix: improve edit page routing

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 
 export default function Page({ children, title }: {children: React.ReactNode, title : string}) {
+    const url = new URL(document.URL)
+
+    const slug = url.pathname.substring(1)
+
     return <main className="m-4">
         <div className="container mx-auto">
 
             <div className="mb-4">
-                <Link href={`https://github.com/baseplate-wiki/baseplate-wiki/tree/main/content/${title.toLocaleLowerCase()}.mdx`}>[ edit this page ]</Link>
-                <Link href={`https://raw.githubusercontent.com/baseplate-wiki/baseplate-wiki/refs/heads/main/content/${title.toLocaleLowerCase()}.mdx`}>[ view raw ]</Link>
+                <Link target="_blank" href={`https://github.com/baseplate-wiki/baseplate-wiki/tree/main/content/${slug}.mdx`}>[ edit this page ]</Link>
+                <Link target="_blank" href={`https://raw.githubusercontent.com/baseplate-wiki/baseplate-wiki/refs/heads/main/content/${slug}.mdx`}>[ view raw ]</Link>
             </div>
 
             <h1 className="font-bold text-3xl tracking-tight mb-2"> {title} </h1>
