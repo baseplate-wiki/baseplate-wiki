2d8d70e51493468e082c315a88a37aab2dfe741c
wlft <100052562+wlft@users.noreply.github.com>
2026-05-24 10:44:07 +1000

    feat: title metadata

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -31,3 +31,27 @@ export default async function RootPage({params} : { params : Promise<params>}) {
 export function generateStaticParams() {
   return all_page_slugs.map((s) => ({ slug: s === 'index' ? [] : s.split('/') }));
 }
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<params>;
+}): Promise<Metadata> {
+  const { slug = [] } = await params;
+  const key = (slug.length === 0 ? "index" : slug.join("/")) as keyof typeof content_registry;
+  const loader = content_registry[key];
+  if (!loader) return {};
+  const mod = (await loader()) as content_module;
+  const meta = mod.meta ?? {};
+  return {
+    title: (meta.title && meta.title + ' - Baseplate Wiki') ?? "Baseplate Wiki",
+    // description: meta.description,
+    // other: meta.date_iso ? { "article:published_time": meta.date_iso } : undefined,
+
+    // icons: {
+    //     icon: [{ url: "/gcd/branding/favicons/woufaviconlight.svg", type: "image/svg" }],
+    //     shortcut: ["/gcd/branding/favicons/woufaviconlight256.ico"],
+    //     apple: [{ url: "/gcd/branding/favicons/woufaviconlight256.png", sizes: "256x256" }],
+    // },
+  };
+}
